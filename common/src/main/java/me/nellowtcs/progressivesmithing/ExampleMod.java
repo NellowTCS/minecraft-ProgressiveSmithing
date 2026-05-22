5a56b6918719ef3e44e66c96962b427d624baa9d
NellowTCS <neeljaiswal23@gmail.com>
2026-05-22 15:39:09 +0000

    It's cool that Architectury offers a template for this!

---
+++ common/src/main/java/me/nellowtcs/progressivesmithing/ExampleMod.java
@@ -0,0 +1,9 @@
+package me.nellowtcs.progressivesmithing;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "progressive_smithing";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
